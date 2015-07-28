797a463f0895a1df3f4b78f9233fd5dad7a9f444
kgaitonde <kgaitonde@GSO-LT-KIGAITON.bradyservices.local>
2015-07-28 10:50:58 -0400

    new layout , vertical spread, scrolling navbar, blog page added

---
+++ web/js/dropdown.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(function() {
+  // Setup drop down menu
+  $('.dropdown-toggle').dropdown();
+ 
+  // Fix input element click problem
+  $('.dropdown input, .dropdown label').click(function(e) {
+    e.stopPropagation();
+  });
+});
